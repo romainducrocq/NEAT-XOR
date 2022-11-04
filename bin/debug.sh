0aab8697823ececea74b15867acf84e377d6eb78
romainducrocq <romain.g.ducrocq@gmail.com>
2022-11-04 11:05:10 +0100

    debug

---
+++ bin/debug.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# https://stackoverflow.com/questions/5134891/how-do-i-use-valgrind-to-find-memory-leaks
+
+args=""
+for ARG in $( echo "${@}" | sed 's/ / /g' )
+do
+    args="${args} ${ARG}"
+done
+
+cd apps/
+valgrind --leak-check=full \
+         --show-leak-kinds=all \
+         --track-origins=yes \
+         --verbose \
+         --log-file=out.txt \
+         ./exec${args}

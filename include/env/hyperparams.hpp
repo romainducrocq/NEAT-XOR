ec1d17c0dd3553bc0ae91f0879c9515736c4c278
romainducrocq <romain.g.ducrocq@gmail.com>
2022-11-09 18:49:50 +0100

    config

--- include/env/hyperparams.hpp
+++ include/env/hyperparams.hpp
@@ -1,8 +1,12 @@
 #ifndef _ENV_HYPERPARAMS_HPP
 #define _ENV_HYPERPARAMS_HPP
 
+#include <unistd.h>
+
 #include <iostream>
 #include <cstddef>
+#include <cstring>
+#include <cstdlib>
 
 template<typename T>
 struct DefaultHyperParams {
@@ -10,78 +14,116 @@ struct DefaultHyperParams {
         XOR
     };
 
-    static size_t INPUTS;
-    static size_t OUTPUTS;
+    const static size_t INPUTS;
+    const static size_t OUTPUTS;
 
-    static size_t LIM_HIDDEN;
+    const static size_t LIM_HIDDEN;
 
-    static float MUTATE_WEIGHT_RATE;
-    static float MUTATE_GENE_RATE;
-    static float MUTATE_LINK_RATE;
-    static float MUTATE_BIAS_RATE;
-    static float MUTATE_NEURON_RATE;
-    static float MUTATE_ENABLE_RATE;
-    static float MUTATE_DISABLE_RATE;
-    static float MUTATE_OFFSET_SIZE;
+    const static float MUTATE_WEIGHT_RATE;
+    const static float MUTATE_GENE_RATE;
+    const static float MUTATE_LINK_RATE;
+    const static float MUTATE_BIAS_RATE;
+    const static float MUTATE_NEURON_RATE;
+    const static float MUTATE_ENABLE_RATE;
+    const static float MUTATE_DISABLE_RATE;
+    const static float MUTATE_OFFSET_SIZE;
 
-    static float DELTA_WEIGHTS;
-    static float DELTA_DISJOINT;
-    static float DELTA_THRESHOLD;
+    const static float DELTA_WEIGHTS;
+    const static float DELTA_DISJOINT;
+    const static float DELTA_THRESHOLD;
 
-    static float CROSSOVER_PROB;
+    const static float CROSSOVER_PROB;
+    const static size_t STALE_SPECIES;
 
-    static size_t STALE_SPECIES;
+    const static size_t ACT_REPEAT;
+    const static size_t MAX_NOOP;
+    const static size_t MAX_STEP;
+    const static size_t EPOCHS;
+
     static size_t POPULATION_SIZE;
-
-    static size_t ACT_REPEAT;
-    static size_t MAX_NOOP;
-    static size_t MAX_STEP;
-    static size_t EPOCHS;
     static size_t GENERATIONS;
+
+    static bool argParse(int argc, char** argv)
+    {
+        // https://github.com/gnif/LookingGlass/blob/c0c63fd93bf999b6601a782fec8b56e9133388cc/client/main.c#L1391
+
+        for(;;){
+            switch(getopt(argc, argv, "h:g:p:")){
+                case '?': // help
+                case 'h':
+                default :
+                    std::cerr << "usage: apps/exec [-h] [-g GEN] [-p POP] \n";
+                    std::cerr << "\n";
+                    std::cerr << "NEAT XOR                                \n";
+                    std::cerr << "\n";
+                    std::cerr << "optional args:                          \n";
+                    std::cerr << "  -h      Print this help and exit      \n";
+                    std::cerr << "  -g GEN  Set generation number         \n";
+                    std::cerr << "  -p POP  Set population size           \n";
+
+                    return false;
+
+                case -1:
+                    break;
+
+                case 'g': // generations
+                    DefaultHyperParams<T>::GENERATIONS = static_cast<size_t>(std::atoi(optarg));
+                    continue;
+
+                case 'p': // population size
+                    DefaultHyperParams<T>::POPULATION_SIZE = static_cast<size_t>(std::atoi(optarg));
+                    continue;
+            }
+            break;
+        }
+
+        return true;
+    }
 };
 
 template<typename T>
-size_t DefaultHyperParams<T>::INPUTS = 2;
+const size_t DefaultHyperParams<T>::INPUTS = 2;
 template<typename T>
-size_t DefaultHyperParams<T>::OUTPUTS = 1;
+const size_t DefaultHyperParams<T>::OUTPUTS = 1;
 template<typename T>
-size_t DefaultHyperParams<T>::LIM_HIDDEN = 1000000;
+const size_t DefaultHyperParams<T>::LIM_HIDDEN = 1000000;
 template<typename T>
-float DefaultHyperParams<T>::MUTATE_WEIGHT_RATE = 0.1f;
+const float DefaultHyperParams<T>::MUTATE_WEIGHT_RATE = 0.1f;
 template<typename T>
-float DefaultHyperParams<T>::MUTATE_GENE_RATE = 0.25f;
+const float DefaultHyperParams<T>::MUTATE_GENE_RATE = 0.25f;
 template<typename T>
-float DefaultHyperParams<T>::MUTATE_LINK_RATE = 2.f;
+const float DefaultHyperParams<T>::MUTATE_LINK_RATE = 2.f;
 template<typename T>
-float DefaultHyperParams<T>::MUTATE_BIAS_RATE = 0.4f;
+const float DefaultHyperParams<T>::MUTATE_BIAS_RATE = 0.4f;
 template<typename T>
-float DefaultHyperParams<T>::MUTATE_NEURON_RATE = 0.5f;
+const float DefaultHyperParams<T>::MUTATE_NEURON_RATE = 0.5f;
 template<typename T>
-float DefaultHyperParams<T>::MUTATE_ENABLE_RATE = 0.2f;
+const float DefaultHyperParams<T>::MUTATE_ENABLE_RATE = 0.2f;
 template<typename T>
-float DefaultHyperParams<T>::MUTATE_DISABLE_RATE = 0.4f;
+const float DefaultHyperParams<T>::MUTATE_DISABLE_RATE = 0.4f;
 template<typename T>
-float DefaultHyperParams<T>::MUTATE_OFFSET_SIZE = 0.1f;
+const float DefaultHyperParams<T>::MUTATE_OFFSET_SIZE = 0.1f;
 template<typename T>
-float DefaultHyperParams<T>::DELTA_DISJOINT = 2.f;
+const float DefaultHyperParams<T>::DELTA_DISJOINT = 2.f;
 template<typename T>
-float DefaultHyperParams<T>::DELTA_WEIGHTS = 0.4f;
+const float DefaultHyperParams<T>::DELTA_WEIGHTS = 0.4f;
 template<typename T>
-float DefaultHyperParams<T>::DELTA_THRESHOLD = 1.f;
+const float DefaultHyperParams<T>::DELTA_THRESHOLD = 1.f;
 template<typename T>
-float DefaultHyperParams<T>::CROSSOVER_PROB = 0.75f;
+const float DefaultHyperParams<T>::CROSSOVER_PROB = 0.75f;
 template<typename T>
-size_t DefaultHyperParams<T>::STALE_SPECIES = 15;
+const size_t DefaultHyperParams<T>::STALE_SPECIES = 15;
+template<typename T>
+const size_t DefaultHyperParams<T>::ACT_REPEAT = 0;
+template<typename T>
+const size_t DefaultHyperParams<T>::MAX_NOOP = 0;
+template<typename T>
+const size_t DefaultHyperParams<T>::MAX_STEP = 0;
+template<typename T>
+const size_t DefaultHyperParams<T>::EPOCHS = 10;
+
 template<typename T>
 size_t DefaultHyperParams<T>::POPULATION_SIZE = 150;
-template<typename T>
-size_t DefaultHyperParams<T>::ACT_REPEAT = 0;
-template<typename T>
-size_t DefaultHyperParams<T>::MAX_NOOP = 0;
-template<typename T>
-size_t DefaultHyperParams<T>::MAX_STEP = 0;
-template<typename T>
-size_t DefaultHyperParams<T>::EPOCHS = 10;
 template<typename T>
 size_t DefaultHyperParams<T>::GENERATIONS = 300;
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,39 @@
 #include <cstdlib>
 #include <ctime>
 
+#include "env/hyperparams.hpp"
 #include "env/env.hpp"
 #include "env/env/utils.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
     std::srand(time(nullptr));
 
-    Env env;
+    if(HyperParams::argParse(argc, argv)) {
+        Env env;
 
-    {
-        Timer timer;
+        {
+            Timer timer;
 
-        std::cout << "\n";
-        std::cout << "-------------------------------TRAIN-------------------------------" << "\n";
-        std::cout << "\n";
+            std::cout << "\n";
+            std::cout << "-------------------------------TRAIN-------------------------------" << "\n";
+            std::cout << "\n";
 
-        env.train();
+            env.train();
+        }
+
+        {
+            Timer timer;
+
+            std::cout << "\n";
+            std::cout << "-------------------------------EVAL--------------------------------" << "\n";
+            std::cout << "\n";
+
+            env.eval();
+        }
+
+        return 0;
     }
 
-    {
-        Timer timer;
-
-        std::cout << "\n";
-        std::cout << "-------------------------------EVAL--------------------------------" << "\n";
-        std::cout << "\n";
-
-        env.eval();
-    }
-
-    return 0;
+    return 1;
 }
